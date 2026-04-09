5876420d9c20b98246146564c33800c03488a1dd
Sayali Chitale <sayalichitale8032@gmail.com>
2026-04-09 12:59:54 +0530

    comparison.js: added notes for null vs undefined

--- comparison.js
+++ comparison.js
@@ -10,6 +10,18 @@
 
 // console.log(null > 0);
 // console.log(null == 0);
-// console.log(null >= 0);
+// console.log(null >= 0);W
 
-console.log(undefined === 0);
+console.log(null == undefined); // true
+console.log(null === undefined); // false 
+
+// undefined is the value which is assigned automatically by javascript when there is no value.
+// typeof undefined is undefined
+let a;
+console.log(a); // undefined
+
+// null also represents empty value but this value is explicitly assigned by the user
+// typeof null is object which is a universal legacy bug in javascript
+let b = null;
+console.log(b);
+
